69133676d56fe7f2a17b9545560beff605fcaaf0
myyy505 <helmiafandi25@gmail.com>
2026-08-01 22:16:32 +0700

    fix(sanctum): add mybarber.my.id domain to Sanctum stateful configuration

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,mybarber.my.id,www.mybarber.my.id,::1',
         Sanctum::currentApplicationUrlWithPort(),
     ))),
 
